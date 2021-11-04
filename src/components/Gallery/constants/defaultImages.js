3dae145fe70f2d3e72c1fdf266306cec4a4f514c
caseyfoster <me@caseyfoster.net>
2021-11-04 12:37:02 -0400

    changed description, onhover

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -15,42 +15,36 @@ import thumb06 from '../../../assets/images/thumbs/06.jpg'
 export const DEFAULT_IMAGES = [
     {
       id: '1',
-      source: full01,
       thumbnail: thumb01,
       caption: 'KovaaKs',
       description: 'The world’s best aim trainer, trusted by top pros, streamers, and players like you. Upgrade your FPS skills with over 15,000 player-created scenarios, infinite customization, cloned game physics, sharable playlists, and guided training and analysis.',
     },
     {
       id: '2',
-      source: full02,
       thumbnail: thumb02,
       caption: 'Refrag',
       description: 'Refrag is the Counter-Strike: Global Offensive gamechanger. Through multiple training tools created by and with professional players, we have managed to create the ultimate CS:GO training suite.',
     },
     {
       id: '3',
-      source: full03,
       thumbnail: thumb03,
       caption: 'CSGOHUB Training Map',
       description: 'Multi training CSGO map used by 3million unique players around the world in partnership with ESL',
     },
     {
       id: '4',
-      source: full04,
       thumbnail: thumb04,
       caption: 'Pregame',
       description: 'Competitive CSGO League and Pick Up Game System in partnership with FACEIT',
     },
     {
       id: '5',
-      source: full05,
       thumbnail: thumb05,
       caption: 'Boomeo',
       description: 'The sucessor web based video training platform to NetcodeGuides in partnership with WME|IMG ELEAGUE',
     },
     {
       id: '6',
-      source: full06,
       thumbnail: thumb06,
       caption: 'NetcodeGuides',
       description: 'The largest CSGO training platform used by hundreds of thoughsands of players around the world',
