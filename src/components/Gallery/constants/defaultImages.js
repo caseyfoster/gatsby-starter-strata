df98215249e75e6f281cb1d3a2df58a3933fb17b
caseyfoster <me@caseyfoster.net>
2021-11-05 09:10:37 -0400

    changeess to webp

--- src/components/Gallery/constants/defaultImages.js
+++ src/components/Gallery/constants/defaultImages.js
@@ -4,12 +4,12 @@ import full03 from '../../../assets/images/fulls/03.jpg'
 import full04 from '../../../assets/images/fulls/04.jpg'
 import full05 from '../../../assets/images/fulls/05.jpg'
 import full06 from '../../../assets/images/fulls/06.jpg'
-import thumb02 from '../../../assets/images/thumbs/02.jpg'
-import thumb01 from '../../../assets/images/thumbs/01.jpg'
-import thumb03 from '../../../assets/images/thumbs/03.jpg'
-import thumb04 from '../../../assets/images/thumbs/04.jpg'
-import thumb05 from '../../../assets/images/thumbs/05.jpg'
-import thumb06 from '../../../assets/images/thumbs/06.jpg'
+import thumb02 from '../../../assets/images/thumbs/02.webp'
+import thumb01 from '../../../assets/images/thumbs/01.webp'
+import thumb03 from '../../../assets/images/thumbs/03.webp'
+import thumb04 from '../../../assets/images/thumbs/04.webp'
+import thumb05 from '../../../assets/images/thumbs/05.webp'
+import thumb06 from '../../../assets/images/thumbs/06.webp'
 
 
 export const DEFAULT_IMAGES = [
