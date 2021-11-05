8594108e96e88ea66e30b829f9d73f2b36d3a66b
caseyfoster <me@caseyfoster.net>
2021-11-05 10:01:49 -0400

    name change again

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'Casey-Foster-Portfolio',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
