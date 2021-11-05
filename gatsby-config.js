4de9270963cfc4a0732b057fcc2ef1dde1e9253c
caseyfoster <me@caseyfoster.net>
2021-11-05 10:03:26 -0400

    app name change

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Casey-Foster-Portfolio',
+        name: 'Casey Foster - Portfolio',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
